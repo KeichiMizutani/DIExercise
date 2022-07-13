7c78a0c862fadfddd47766c4676d09511f551eed
水谷恵知 <>
2022-07-13 13:57:32 +0900

    [add] zxing test

--- Assets/Scripts/TestQRGenerator.cs
+++ Assets/Scripts/TestQRGenerator.cs
@@ -1,19 +1,45 @@
 using System;
 using System.Security.Cryptography;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
+
 using UnityEngine;
+//using ZXing;        //QRコード作成に必要
+//using ZXing.QrCode; //QRコード作成に必要
 
 public class TestQRGenerator : MonoBehaviour
 {
-    
-    string fishID = "WakayamaMaguro";
+    private const string PublickeyFormat = "MMddHHmm";
+    private const string SecretkeyFormat = "HHmm";
+    private const string SecretkyHead = "mitt";
+    private string fishID = "jp_1_001";//"wakayama_aeonmal_your'sland";
 
-    // Update is called once per frame
+    [SerializeField]private SpriteRenderer qrSprite;//最終的に表示するSpriteRendererオブジェクト
+    private Texture2D encodedQRTextire;//エンコードして出来たQRコードのTxture2Dが入る
+    
+    private int qrTxtureW = 256;//作成するテクスチャサイズ
+    private int qrTxtureH = 256;//作成するテクスチャサイズ
+
+    private string qrString;
+    
+    private float timer = 0.0f;
+
+    private void Start()
+    {
+        encodedQRTextire = new Texture2D(qrTxtureW, qrTxtureH);
+    }
+
     void Update()
     {
+        timer += Time.deltaTime;
+        if (timer > 60.0f)
+        {
+            timer = 0.0f;
+            qrString = Encrypt(fishID);
+            //エンコード処理
+            //var color32 = Encode(ImageLink, EncodedQRTextire.width, EncodedQRTextire.height);
+        }
+        
         if (Input.GetKeyDown(KeyCode.Q))
         {
             string encrypted = Encrypt(fishID);
@@ -30,24 +56,22 @@ public class TestQRGenerator : MonoBehaviour
         {
             string returnStr = "";
             DateTime dt = DateTime.UtcNow;
-            string publickey = dt.ToString("MMddHHmm");
-            string secretkey = dt.ToString("yyyyHHmm");
+            string publickey = dt.ToString(PublickeyFormat);
+            string secretkey = SecretkyHead + dt.ToString(SecretkeyFormat);
             //string secretkey = dt.ToString("yyyyHHmm");
             //Debug.Log("publickey:" + publickey);
             //Debug.Log("secretkey:" + secretkey);
-            
-            byte[] secretkeyByte = { };
-            secretkeyByte = System.Text.Encoding.UTF8.GetBytes(secretkey);
-            byte[] publickeybyte = { };
-            publickeybyte = System.Text.Encoding.UTF8.GetBytes(publickey);
+
+            byte[] secretkeyByte = Encoding.UTF8.GetBytes(secretkey);
+            byte[] publickeyByte = Encoding.UTF8.GetBytes(publickey);
             MemoryStream ms = null;
             CryptoStream cs = null;
-            byte[] inputbyteArray = System.Text.Encoding.UTF8.GetBytes(textToEncrypt);
+            byte[] inputbyteArray = Encoding.UTF8.GetBytes(textToEncrypt);
             
             using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
             {
                 ms = new MemoryStream();
-                cs = new CryptoStream(ms, des.CreateEncryptor(publickeybyte, secretkeyByte), CryptoStreamMode.Write);
+                cs = new CryptoStream(ms, des.CreateEncryptor(publickeyByte, secretkeyByte), CryptoStreamMode.Write);
                 cs.Write(inputbyteArray, 0, inputbyteArray.Length);
                 cs.FlushFinalBlock();
                 returnStr = Convert.ToBase64String(ms.ToArray());
@@ -67,24 +91,21 @@ public class TestQRGenerator : MonoBehaviour
         {
             string returnStr = "";
             DateTime dt = DateTime.UtcNow;
-            string publickey = dt.ToString("MMddHHmm");
-            string secretkey = dt.ToString("yyyyHHmm");
+            string publickey = dt.ToString(PublickeyFormat);
+            string secretkey = SecretkyHead + dt.ToString(SecretkeyFormat);
             // string secretkey = dt.ToString("yyyyHHmm");
             //Debug.Log("publickey:" + publickey);
             //Debug.Log("secretkey:" + secretkey);
 
-            byte[] privatekeyByte = { };
-            privatekeyByte = System.Text.Encoding.UTF8.GetBytes(secretkey);
-            byte[] publickeybyte = { };
-            publickeybyte = System.Text.Encoding.UTF8.GetBytes(publickey);
+            byte[] privatekeyByte = Encoding.UTF8.GetBytes(secretkey);
+            byte[] publickeyByte = Encoding.UTF8.GetBytes(publickey);
             MemoryStream ms = null;
             CryptoStream cs = null;
-            byte[] inputbyteArray = new byte[textToDecrypt.Replace(" ", "+").Length];
-            inputbyteArray = Convert.FromBase64String(textToDecrypt.Replace(" ", "+"));
+            byte[] inputbyteArray =Convert.FromBase64String(textToDecrypt.Replace(" ", "+"));
             using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
             {
                 ms = new MemoryStream();
-                cs = new CryptoStream(ms, des.CreateDecryptor(publickeybyte, privatekeyByte), CryptoStreamMode.Write);
+                cs = new CryptoStream(ms, des.CreateDecryptor(publickeyByte, privatekeyByte), CryptoStreamMode.Write);
                 cs.Write(inputbyteArray, 0, inputbyteArray.Length);
                 cs.FlushFinalBlock();
                 Encoding encoding = Encoding.UTF8;
