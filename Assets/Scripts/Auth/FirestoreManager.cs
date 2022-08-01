51dffec59cda8161a9c7d706e979d428f3e97b96
KeichiMizutani <m.keichi16256@gmail.com>
2022-08-02 08:07:49 +0900

    ビルドデータ落ちる問題

---
+++ Assets/Scripts/Auth/FirestoreManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirestoreManager : SingletonMonoBehaviour<FirestoreManager>
+{
+    
+    
+    protected override void Awake()
+    {
+        base.Awake();
+
+        //var db = FirebaseFirestore.DefaultInstance;
+        
+    }
+}
